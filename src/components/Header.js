068aa755d248d0d0d3d930cae1ae339f2f9d68dc
nenu <nenu@nenu.fr>
2017-08-08 15:02:20 +0200

    replace <a> tags by <Link> tags

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,19 @@
 // import react,
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 // create and export component
 export default class Header extends Component {
   render() {
     return(
       <nav className="navbar navbar-inverse navbar-fixed-top">
-        <a className="navbar-brand" href="/">Firebase Boilerplate</a>
+        <Link className="navbar-brand" href="/">Firebase Boilerplate</Link>
         <ul className="nav navbar-nav">
           <li className="active">
-            <a href="/">Home</a>
+            <Link href="/">Home</Link>
           </li>
           <li>
-            <a href="/">Auth</a>
+            <Link href="/">Auth</Link>
           </li>
           <p className="navbar-text navbar-right">Signed in as:</p>
         </ul>
