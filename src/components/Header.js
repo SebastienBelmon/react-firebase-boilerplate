b07831d2b0b69516465aa32cb00d2c435f2b795c
nenu <nenu@nenu.fr>
2017-08-08 15:02:32 +0200

    replaced href with to

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,13 @@ export default class Header extends Component {
   render() {
     return(
       <nav className="navbar navbar-inverse navbar-fixed-top">
-        <Link className="navbar-brand" href="/">Firebase Boilerplate</Link>
+        <Link className="navbar-brand" to="/">Firebase Boilerplate</Link>
         <ul className="nav navbar-nav">
           <li className="active">
-            <Link href="/">Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link href="/">Auth</Link>
+            <Link to="#">Auth</Link>
           </li>
           <p className="navbar-text navbar-right">Signed in as:</p>
         </ul>
